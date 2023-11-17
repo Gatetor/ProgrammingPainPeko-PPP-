6227ebcc4c1771aba186bf3dac62841e3505bb4e
Gatetor <gapulimar@gmail.com>
2023-11-17 13:11:58 +0100

    furula a medias
    hay que arreglar el setter de cantidad para poder ingresarla en la cuenta

--- TitularYCuentasStrings/src/Cuenta.java
+++ TitularYCuentasStrings/src/Cuenta.java
@@ -1,13 +1,47 @@
+import java.util.Scanner;
+
 public class Cuenta {
     private int id;
     private double saldo;
     private Titular titular;
+    private double cantidad;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Titular getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Titular titular) {
+        this.titular = titular;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
 
     public Cuenta(int id, double saldo, Titular titular) {
-       this.id = id;
-       this.saldo = saldo;
-       this.titular = titular;
+        this.id = id;
+        this.saldo = saldo;
+        this.titular = titular;
     }
 
     public String toStringCuenta() {
@@ -20,18 +54,28 @@ public class Cuenta {
         return todo;
     }
 
-    public static void main(String[] args) throws Exception {
-    
-        Cuenta cuenta1;
-        Titular titular1;
-    
-        titular1 = new Titular("0000001M", "Alv" , 3.14 , "Madre Loco");
-        cuenta1 = new Cuenta(1, 3.14, titular1);
-    
-        System.out.println(cuenta1.mostrar());
-    
+    public double ingresar() {
+        this.setCantidad(cantidad);
+        return saldo;
     }
 
+    public static void main(String[] args) throws Exception {
 
+        Scanner teclado = new Scanner(System.in);
+
+        Cuenta cuenta1;
+        Titular titular1;
+
+        titular1 = new Titular("0000001M", "Alv", 3.14, "Madre Loco");
+        cuenta1 = new Cuenta(1, 3.14, titular1);
+
+        System.out.println("Cantidad a ingresar?");
+        System.out.println("Cantidad actual: " + cuenta1.saldo);
+
+        this.cantidad = teclado.nextDouble();
+
+        System.out.println(cuenta1.mostrar());
+
+    }
 
 }
