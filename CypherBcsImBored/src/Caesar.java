913cb7241118a0706a830395c234beafdbc53b6a
Gatetor <gapulimar@gmail.com>
2023-12-01 13:02:20 +0100

    norecuerdo^2

--- CypherBcsImBored/src/Caesar.java
+++ CypherBcsImBored/src/Caesar.java
@@ -78,4 +78,8 @@ public class Caesar {
         return textoDescifrado;
     }
 
+    
+
+
+
 }
--- Estructuras de datos/PrimerArray/src/WithFor.java
+++ Estructuras de datos/PrimerArray/src/WithFor.java
@@ -25,7 +25,7 @@ public class WithFor {
 
         System.out.println("Introduce frase para cifrar");
         poggers = teclado.nextLine();
-    
+
         int arrayLength = poggers.length();
 
         // allocates memory for 10 integers
@@ -45,9 +45,10 @@ public class WithFor {
         anArray[7] = 800;
         anArray[8] = 900;
         anArray[9] = 1000;
-        // FIXME: Esto se queda irrelevante con los metodos de mas adelante, pero venian en el ejemplo
+        // FIXME: Esto se queda irrelevante con los metodos de mas adelante, pero venian
+        // en el ejemplo
 
-        //TODO: cifrado cesar con arrays
+        // TODO: cifrado cesar con arrays
 
         for (int i = 0; i < anArray.length; i++) {
             System.out.println("Introduzca valor: ");
@@ -72,19 +73,19 @@ public class WithFor {
         // mayor = anArray[0];
 
         // for (int i = 0; i < anArray.length; i++) {
-        //     if (anArray[i] > mayor) {
-        //         mayor = anArray[i];
-        //     }
+        // if (anArray[i] > mayor) {
+        // mayor = anArray[i];
+        // }
         // }
 
-        //sysout con el metodo, pasandole el array
+        // sysout con el metodo, pasandole el array
         System.out.println("Mayor: " + mayor(anArray));
 
         // rotateBanana(anArray);
 
         // for (int valor : anArray) {
-        //     Thread.sleep(300);
-        //     System.out.println("Valor: " + valor);
+        // Thread.sleep(300);
+        // System.out.println("Valor: " + valor);
         // }
 
         // System.out.println("El mas tocho: " + mayor);
@@ -100,18 +101,20 @@ public class WithFor {
             System.out.println("No ta ordenao :(");
         }
 
-        int[] array1 = {1,2,3};
-        int[] array2 = {4,5,6};
+        int[] array1 = { 1, 2, 3 };
+        int[] array2 = { 4, 5, 6 };
         int[] sumaArrays = sumaVectores(array1, array2);
 
         System.out.println("**********SUMA**********");
-        //Arrays.toString pasandole el objeto de la suma para poder mostrarlo por pantalla
-        //sin que te de la direccion de memoria y te quedes to loco pensando "wtf did just happen"
+        // Arrays.toString pasandole el objeto de la suma para poder mostrarlo por
+        // pantalla
+        // sin que te de la direccion de memoria y te quedes to loco pensando "wtf did
+        // just happen"
         System.out.println(Arrays.toString(sumaArrays));
 
     }
 
-    //lo de coger el mayor del array hecho un metodo
+    // lo de coger el mayor del array hecho un metodo
     public static int mayor(int[] valores) {
         int mayorinside = valores[0];
 
@@ -123,12 +126,12 @@ public class WithFor {
         return mayorinside;
     }
 
-    //contar los positivos en el array
+    // contar los positivos en el array
     public static int positivos(int[] anArray) {
-        
+
         int contador = 0;
 
-        for (int valor : anArray ) {
+        for (int valor : anArray) {
             if (valor > 0) {
                 contador++;
             }
@@ -136,71 +139,70 @@ public class WithFor {
         return contador;
     }
 
-    //rotar por la derecha para que los numeros ciclen
+    // rotar por la derecha para que los numeros ciclen
     public static int[] rotateBanana(int[] elArray) {
-        //creamos un auxiliar para guardar el ultimo numero y no joder todo
-        //ya te vale, seguro que lo ibas a hacer mal
+        // creamos un auxiliar para guardar el ultimo numero y no joder todo
+        // ya te vale, seguro que lo ibas a hacer mal
         int auxiliar;
-        auxiliar = elArray[elArray.length-1];
+        auxiliar = elArray[elArray.length - 1];
 
-        //hacemos todo el proceso, que deja la casilla numero 1 sin re-escribir
-        for (int i = elArray.length-1; i >= 1; i--) {
-            //array-1 es porque si tiene 10 casillas, empieza en la 0 y se para en la 9, hay que restar 1
-            elArray[i] = elArray[i-1];    
-        }   
-        //re-escribimos en casilla 0 para poner el que estaba en la ultima posicion
+        // hacemos todo el proceso, que deja la casilla numero 1 sin re-escribir
+        for (int i = elArray.length - 1; i >= 1; i--) {
+            // array-1 es porque si tiene 10 casillas, empieza en la 0 y se para en la 9,
+            // hay que restar 1
+            elArray[i] = elArray[i - 1];
+        }
+        // re-escribimos en casilla 0 para poner el que estaba en la ultima posicion
         elArray[0] = auxiliar;
         return elArray;
     }
 
-    public static boolean ordenado(int [] numeros) {
+    public static boolean ordenado(int[] numeros) {
         boolean isInOrder = true;
 
-        for (int i = 0; i < numeros.length-1; i++) {
-            if (numeros[i] > numeros[i+1]) {
+        for (int i = 0; i < numeros.length - 1; i++) {
+            if (numeros[i] > numeros[i + 1]) {
                 isInOrder = false;
             }
         }
-        return isInOrder; 
+        return isInOrder;
     }
 
     private static int[] sumaVectores(int[] vector1, int[] vector2) {
         int[] suma = null;
 
         if ((vector1 == null)
-            || (vector2 == null)
-            || (vector1.length != vector2.length)) {
+                || (vector2 == null)
+                || (vector1.length != vector2.length)) {
             return suma;
         }
-
 
         suma = new int[vector1.length];
 
         for (int i = 0; i < suma.length; i++) {
-            suma[i] = vector1[i] + vector2[i]; 
+            suma[i] = vector1[i] + vector2[i];
         }
 
         return suma;
     }
 
-    //metodo que reciba array de numeros naturales, que devuelva
-    //cuantos numeros primos hay en el array
-
+    // metodo que reciba array de numeros naturales, que devuelva
+    // cuantos numeros primos hay en el array
 
     public class contaPrimos {
 
-        private static boolean esPrimo (int[] numeros) {
+        private static boolean esPrimo(int[] numeros) {
             int resto, cont = 0;
             boolean esPrimo = true;
 
             for (int i = 0; i < numeros[i]; i++) {
-    
+
                 if (numeros[i] == 2 || numeros[i] == 1) {
                     return true;
                 } else {
                     while (cont < numeros[i] && esPrimo == true) {
                         resto = numeros[i] % cont;
-    
+
                         if (resto == 0) {
                             return false;
                         }
@@ -210,10 +212,10 @@ public class WithFor {
             }
             return true;
         }
-    
-        public static int contarPrimos (int[] numeros) {
+
+        public static int contarPrimos(int[] numeros) {
             int contPrimos = 0;
-    
+
             for (int i = 0; i < numeros.length; i++) {
                 if (esPrimo(numeros)) {
                     contPrimos++;
@@ -222,29 +224,62 @@ public class WithFor {
             return contPrimos;
         }
 
-
-    
-        //TODO: Aplicar esto en mi main
+        // TODO: Aplicar esto en mi main
 
         // public static void main(String[] args) {
-    
-        //     Scanner sc = new Scanner (System.in);
-    
-        //     int[] anArray = new int[10];
-    
-        //     for (int i=0;i<anArray.length;i++) {
-        //         System.out.println ("Valor " + i);
-        //             anArray[i] = sc.nextInt();
-        //     }
-        //     System.out.println (contarPrimos(anArray));
-    
-        //     for (int i: anArray) {
-        //         System.out.println ("Valor: " + i);
-        //     }
-        //     sc.close();
+
+        // Scanner sc = new Scanner (System.in);
+
+        // int[] anArray = new int[10];
+
+        // for (int i=0;i<anArray.length;i++) {
+        // System.out.println ("Valor " + i);
+        // anArray[i] = sc.nextInt();
+        // }
+        // System.out.println (contarPrimos(anArray));
+
+        // for (int i: anArray) {
+        // System.out.println ("Valor: " + i);
+        // }
+        // sc.close();
         // }
     }
 
+    // public static String cifrar(String texto) {
+    // char
+
+    // return resultado;
+    // }
+
+    public static char[] cifrar(char[] texto) {
+        char[] resultado = new char[texto.length];
+        String textoACifrar;
+        String textoCifrado;
+        char letra;
+
+        for (int i = 0; i < texto.length; i++) {
+            letra = texto[i];
+            if (letra == 'x') {
+                texto[i] = 'a';
+            } else {
+                if (letra == 'y') {
+                    texto[i] = 'b';
+                } else {
+                    if (letra == 'z') {
+                        texto[i] = 'c';
+                    } else {
+                        texto[i] = (char) (letra + 3);
+                    }
+                }
+            }
+        }
+
+        // textoACifrar = new String(texto);
+        // textoCifrado = cifrar(textoACifrar);
+
+        // resultado = textoCifrado.toCharArray();
+
+        return resultado;
+    }
 
 }
-
