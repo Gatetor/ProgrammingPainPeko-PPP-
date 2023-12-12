222303b84bc03550cb43bdef0729947d8b0ac072
Gatetor <gapulimar@gmail.com>
2023-12-12 12:02:45 +0100

    Creacion

--- Estructuras de datos/AlumnoNotas/src/Alumno.java
+++ Estructuras de datos/AlumnoNotas/src/Alumno.java
@@ -1,7 +1,18 @@
 public class Alumno {
     private String name;
     private double midnota;
+    private int id;
 
-    
+    public double notaMasAlta() {
+        double notaMasAlta;
+
+        return notaMasAlta;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
 
 }
---
+++ Estructuras de datos/AlumnoNotas/src/Asignatura.java
@@ -0,0 +1,8 @@
+public class Asignatura {
+private Asignatura asignatura1;    
+private Asignatura asignatura2;
+private Asignatura asignatura3;
+
+
+
+}
