f777c4435eae4a7b28d029435bb555a4e6889916
Gatetor <gapulimar@gmail.com>
2023-12-14 12:57:48 +0100

    Metodo para buscar el menor (inefficient af)

---
+++ Estructuras de datos/InefficientOrder/src/BuscarMenores.java
@@ -0,0 +1,30 @@
+public class BuscarMenores { // KEKW nombre
+    public static void ordenar(int[] numeros) throws Exception {
+
+        int auxiliar, menorPos;
+        int menor;
+
+        for (int i = 0; i < numeros.length - 1; i++) {
+            // buscar el mas pequeño y ubicarlo
+            menor = numeros[i]; // inicia con i para ahorrar problemas al comparar
+            menorPos = i;
+
+            for (int j = i + 1; j < numeros.length; j++) {
+
+                if (numeros[j] < menor) {
+                    menor = numeros[j];
+                    menorPos = j;
+                }
+
+            }
+            // intercambio con el mas pequeño
+            auxiliar = numeros[i];
+            numeros[i] = menor;
+            numeros[menorPos] = auxiliar;
+
+        }
+
+        // TODO: autorizacion a nacho
+
+    }
+}
