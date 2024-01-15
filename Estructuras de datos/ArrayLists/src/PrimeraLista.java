4b22c2b5f613d9e91d062c390116ad2a676c9d57
Gatetor <gapulimar@gmail.com>
2024-01-15 09:11:17 +0100

    idk

---
+++ Estructuras de datos/ArrayLists/src/PrimeraLista.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+
+    public static void invertir(ArrayList<String> lista) throws Exception {
+        
+        String aux;
+
+        for(int i=0 ){
+            
+        }
+
+
+
+    }
+
---
+++ Estructuras de datos/ListaCOsasIDk/src/listaEmpresas.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class listaEmpresas {
+
+    ArrayList<Persona> listaPersonas;
+    ArrayList<Empresa> listaEmpresas;
+
+
+    
+
+    public void altaPersona(Persona p) {
+
+    }
+
+    public void altaEmpresa(Empresa e) {
+
+    }
+
+    public boolean baja() {
+        boolean yesnt = false;
+
+        return yesnt;
+    }
+    
+    public boolean modyPhone(int id, String nuevoPhone) {
+        boolean yesnt = false;
+
+        return yesnt;
+    }
+
+    public void listarDatos() {
+        //mostrar los datos de la lista
+    }
+
+}
