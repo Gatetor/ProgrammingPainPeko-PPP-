8ae18a80bcc439e91720a825abeaa55c3c63e985
Gatetor <gapulimar@gmail.com>
2024-01-19 10:41:50 +0100

    bc

---
+++ Estructuras de datos/PilotosList/src/Piloto.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Piloto {
+    
+    private String nombre, apellido, nacionalidad;
+    ArrayList
+
+    public Piloto(String naci, String name, String surname, String runs) {
+
+        this.carreras = runs;
+        this.nacionalidad = naci;
+        this.nombre = name;
+        this.apellido = surname;
+        this.carreras = runs;
+
+    }
+
+}
+
+//Integer.parseInt(); dentro de () va el nombre del Scanner que tiene que pillar
