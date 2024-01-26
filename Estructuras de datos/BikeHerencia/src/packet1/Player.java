ee2a7e0c8ede016ad41ce32423d3d69dc7e4a899
Gatetor <gapulimar@gmail.com>
2024-01-26 11:37:07 +0100

    pog

---
+++ Estructuras de datos/BikeHerencia/src/packet1/Player.java
@@ -0,0 +1,19 @@
+package packet1;
+
+public class Player {
+   
+    private void loco() {
+        System.out.println("poggers");
+    }
+
+    public void dale() {
+        int a = 1;
+        if (a == 1) {
+            loco();
+        }
+    }
+
+    
+
+}
+
---
+++ Estructuras de datos/BikeHerencia/src/packet2/Dragon.java
@@ -0,0 +1,10 @@
+package packet2;
+
+public class Dragon {
+
+    public void alv() {
+
+    System.out.println("Meowzers");
+
+    }
+}
