6858f536813b9eb390aaaf0cb1e38337e610c586
Gatetor <gapulimar@gmail.com>
2024-01-29 10:48:50 +0100

    a

--- Estructuras de datos/BikeHerencia/src/packet2/AlphaSub.java
+++ Estructuras de datos/BikeHerencia/src/packet2/AlphaSub.java
@@ -1,6 +1,11 @@
 package packet2;
 
-public class AlphaSub {
+import packet1.Alpha;
+
+public class AlphaSub extends Alpha {
+    public static void main(String[] args) {
+        System.out.println("FML");
+    }
 
     private int power = 12;
     // protected int sanity = 1;
