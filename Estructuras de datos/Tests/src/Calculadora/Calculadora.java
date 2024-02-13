c115c617bf3402a386be233ee82c53f41266c23f
Gatetor <gapulimar@gmail.com>
2024-02-13 11:59:49 +0100

    tests

---
+++ Estructuras de datos/Tests/src/Calculadora/Calculadora.java
@@ -0,0 +1,13 @@
+package Calculadora;
+
+public class Calculadora {
+
+    public double sumar(double numero1, double numero2) {
+
+        return numero1 + numero2;
+    }
+
+    public double absoluto(double num) {
+        return Math.abs(num);
+    }
+}
---
+++ Estructuras de datos/Tests/src/JUnit5Sample1TestTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Test;
+
+
+public class JUnit5Sample1TestTest {
+    @Test
+    void testAfterAll() {
+
+    }
+
+    @Test
+    void testAfterEach() {
+
+    }
+
+    @Test
+    void testBeforeAll() {
+
+    }
+
+    @Test
+    void testBeforeEach() {
+
+    }
+
+    @Test
+    void testTestMethod1() {
+
+    }
+
+    @Test
+    void testTestMethod2() {
+
+    }
+
+    @Test
+    void testTestMethod3() {
+
+    }
+}
